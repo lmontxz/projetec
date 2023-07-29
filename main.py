318402dca47883ef00945cc82bc74684fb512860
lmontxz <118456918+lmontxz@users.noreply.github.com>
2023-07-29 16:30:22 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('projetec.html')
+    return render_template('home.html')
   
 @app.route('/agendamento')
 def agen():
@@ -16,10 +16,26 @@ def login():
 
 @app.route('/perfil')
 def perfil():
-  return render_template('perfil.html', titulo= 'Perfis recomendados')
+  return render_template('perfil.html', titulo= 'Perfil Profissional')
 
 @app.route('/navegar')
 def navegar():
   return render_template('navegar.html', titulo= 'Perfis recomendados')
+
+@app.route('/psicanalistas')
+def psicanalistas():
+  return render_template('psicanalistas.html', titulo= 'Psicanalistas Disponíveis')
+
+@app.route('/psicologos')
+def psicologos():
+  return render_template('psicologos.html', titulo= 'Psicólogos Disponíveis')
+
+@app.route('/psiquiatras')
+def psiquiatras():
+  return render_template('psiquiatras.html', titulo= 'Psiquiatras Disponíveis')
+
+@app.route('/sobre')
+def sobre():
+  return render_template('sobre.html', titulo= 'Sobre Nós')
 
 app.run(debug = True, host='0.0.0.0', port=100)
