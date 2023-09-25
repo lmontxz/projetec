c83edebc47ad21788b36b152112b3c03dc0cae46
lmontxz <118456918+lmontxz@users.noreply.github.com>
2023-09-24 21:32:57 -0300

    Update banco.sql

--- banco.sql
+++ banco.sql
@@ -1,11 +1,13 @@
-create database teste;
-use teste;
+CREATE DATABASE af;
+use af;
 
-   CREATE TABLE IF NOT EXISTS clientes (
-       id INT AUTO_INCREMENT PRIMARY KEY,
-       nome VARCHAR(255) NOT NULL,
-       email VARCHAR(255) NOT NULL,
-       senha VARCHAR(255) NOT NULL
-   );
-   
-   select * from clientes;
+CREATE TABLE login (
+    id INT AUTO_INCREMENT primary key,
+    nome VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    senha VARCHAR (255) NOT NULL
+);
+
+SELECT * FROM login;
+
+
